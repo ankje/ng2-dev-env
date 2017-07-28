3ec25fc180854a75b4b6a8897b755b8969b842ee
ankje <ankje@qq.com>
2017-07-29 03:16:23 +0800

    update

--- webpack/config/karma.conf.js
+++ webpack/config/karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function (config) {
   var _config = {
     basePath: '',
 
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine'],    
 
     files: [
       {pattern: './config/karma-test-shim.js', watched: false}
@@ -24,7 +24,7 @@ module.exports = function (config) {
       noInfo: true
     },
 
-    reporters: ['kjhtml'],
+    reporters: ['progress','kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
